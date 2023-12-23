f2cdbf42fa89e29476b1a96cd072b6aad341ef37
Md-Arman01 <137868415+Md-Arman01@users.noreply.github.com>
2023-12-24 02:29:49 +0600

    added vercel json file

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -25,16 +25,48 @@ async function run() {
     const tasksCollections = client.db("task_managementDB").collection("tasks");
 
     // server api
+    app.get("/task", async (req, res) => {
+      const result = await tasksCollections.find().toArray();
+      res.send(result);
+    });
     app.get("/task/:email", async (req, res) => {
       const email = req.params.email;
       const query = { user_email: email };
-      const result = await tasksCollections.find(query).toArray()
+      const result = await tasksCollections.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/task1/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await tasksCollections.findOne(query);
       res.send(result);
     });
 
     app.post("/task", async (req, res) => {
       const taskInfo = req.body;
-      const result = await tasksCollections.insertOne(taskInfo)
+      const result = await tasksCollections.insertOne(taskInfo);
+      res.send(result);
+    });
+    app.put("/task1/:id", async (req, res) => {
+      const id = req.params.id;
+      const updateInfo = req.body;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          title: updateInfo?.title,
+          description: updateInfo?.description,
+          datelines: updateInfo?.datelines,
+          priority: updateInfo?.priority,
+        },
+      };
+      const result = await tasksCollections.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    app.delete("/task/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await tasksCollections.deleteOne(query);
       res.send(result);
     });
 
